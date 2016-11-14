01a577b0e0fd90364abe5950048f17dc15a548e0
Kasia Kowalska <kovalskyyyyy@gmail.com>
2016-11-14 18:58:05 +0100

    Add offer section

--- config.rb
+++ config.rb
@@ -31,6 +31,23 @@ activate :sprockets
 # Helpers
 ###
 
+helpers do
+  # Example usage:
+  #
+  #   image_set_tag_3x "foo_1x.png", alt: "foo"
+  #
+  # Will assume there is a 2x and 3x version and provide those automagically.
+  #
+  def image_set_tag_3x(source, options = {})
+    srcset = [ 2, 3 ].map { |num|
+      name = source.sub("_1x.", "_#{num}x.")
+      "#{image_path(name)} #{num}x"
+    }.join(", ")
+
+    image_tag(source, options.merge(srcset: srcset))
+  end
+end
+
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
